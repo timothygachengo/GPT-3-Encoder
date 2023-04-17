c0a2e8c2612dc6af25d54da14e2cc10437572bfc
Sourcery AI <>
2023-04-17 18:24:53 +0000

    'Refactored by Sourcery'

--- encoder.py
+++ encoder.py
@@ -104,8 +104,9 @@ class Encoder:
 
     def decode(self, tokens):
         text = "".join([self.decoder[token] for token in tokens])
-        text = bytearray([self.byte_decoder[c] for c in text]).decode("utf-8", errors=self.errors)
-        return text
+        return bytearray([self.byte_decoder[c] for c in text]).decode(
+            "utf-8", errors=self.errors
+        )
 
 
 def get_encoder():
